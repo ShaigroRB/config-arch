db63fa47ef8d47226c01253a6398d41c5ed9f957
Robin Boucher <robin.boucher@epita.fr>
2019-10-02 14:20:51 +0200

    install: added i3/config and kitty config for installation

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-files="bash_aliases bash_ascii bashrc_perso gitconfig inputrc vimrc Xresources"
+files="bash_aliases bash_ascii bashrc_perso gitconfig inputrc vimrc Xresources
+i3/"
 
 for file in $files; do
-    cp "${file}" "~/.${file}";
+    cp ${file} ~/.${file} ;
 done
+
+# kitty config
+cp -r kitty/ ~/.config/
 
 echo "if [ -f ~/.bashrc_perso ]; then
     . ~/.bashrc_perso;
