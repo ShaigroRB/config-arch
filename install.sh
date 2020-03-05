4add03647b3be79f92a08645baed6a7816c312eb
Robin Boucher <robin.boucher@epita.fr>
2020-03-05 23:02:19 +0100

    install.sh: added bash_functions

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-files="bash_aliases bash_ascii bashrc_perso gitconfig inputrc vimrc Xresources
-i3/config"
+files="bash_aliases bash_ascii bash_functions bashrc_perso gitconfig inputrc
+vimrc Xresources i3/config"
 
 for file in $files; do
     cp ${file} ~/.${file} ;
